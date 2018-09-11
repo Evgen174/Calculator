a38d4d39a0878e1908845a0b5f9443ba0e6f085b
Evgen174 <40502881+Evgen174@users.noreply.github.com>
2018-09-11 14:23:40 +0300

    Add files via upload

---
+++ Calculator.Test/CalculateTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculator.Test
+{
+    public class CalculateTest
+    {
+        [Theory]
+        [InlineData("(8-6)+(8-7)")]
+        [InlineData("-(8-6)+5")]
+        public void CalculateReshTest(string value)
+        {
+
+            var res = Calculate.CalculatePrimer(value);
+
+            Assert.Equal("3", res);
+        }
+    }
+}
---
+++ Calculator.Test/ParseBracketTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace Calculator.Test
+{
+    public class ParseBracketTest
+    {
+        [Theory]
+        [InlineData("(8-6)+(5-7)")]
+        [InlineData("(3+(8-6))+4")]
+        [InlineData("-(8-6)+1")]
+        public void ParsInStringTest(string value)
+        {
+            
+            var res = ParseBracket.ParsInString(value).Length;
+
+            Assert.Equal(3, res);
+        }
+
+        
+
+    }
+}
